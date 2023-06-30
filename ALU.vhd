acf242992e0faaf95bd08f918728d914e0a42e31
Luca Poli <132078534+LucaPoli59@users.noreply.github.com>
2023-06-30 10:09:15 +0200

    Upload CU, modifica (forse) di altri file

--- ALU.vhd
+++ ALU.vhd
@@ -16,16 +16,14 @@ architecture ALU_bhv of ALU is
 signal MP, Mx4: std_logic_vector(2*Nb -1 downto 0);
 
 begin
-	--MP <= (0.5*A + 2*B) / 2;
-	Mx4 <= std_logic_vector(shift_left(resize(unsigned(B), 2*Nb), 2));
+	--MP <= (0.5*A + 2*B) / 2; 
 	MP <= std_logic_vector(  shift_right( shift_right(resize(unsigned(A), 2*Nb), 1) + shift_left(resize(unsigned(B), 2*Nb), 1) , 1) );
+	Mx4 <= std_logic_vector(shift_left(resize(unsigned(B), 2*Nb), 2));
 	
 	out0 <= MP(Nb-1 downto 0) when (selector = '0') else Mx4(Nb-1 downto 0);
 	out1 <= MP(2*Nb -1 downto Nb) when (selector = '0') else Mx4(2*Nb -1 downto Nb);
 	
 end ALU_bhv;
 
-		
-			
-	
+
 		
---
+++ CU.vhd
@@ -0,0 +1,101 @@
+---------------------------------------------------------------------------------------------------
+-- Deeds (Digital Electronics Education and Design Suite)
+-- VHDL Code generated on (25/06/2023, 17:53:29)
+--      by the Deeds (Finite State Machine Simulator)(Deeds-FsM)
+--      Ver. 2.50.200 (Feb 18, 2022)
+-- Copyright (c) 2002-2022 University of Genoa, Italy
+--      Web Site:  https://www.digitalelectronicsdeeds.com
+---------------------------------------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY testFSM IS
+  PORT( ----------------------------------->Cloclk & CL:
+        clk:    IN std_logic;
+        CL: IN std_logic;
+        ----------------------------------->Inputs:
+        X:  IN std_logic;
+        ----------------------------------->Outputs:
+        ALU_s: OUT std_logic;
+        IOM_s: OUT std_logic;
+        COM_s: OUT std_logic
+        -------------------------------------------
+        );
+END testFSM;
+
+ARCHITECTURE behavioral OF testFSM IS       -- (Behavioral Description)
+  TYPE states is ( state_rx,
+                   state_md,
+                   state_b4,
+                   state_tx );
+  SIGNAL State,
+         Next_State: states;
+BEGIN
+
+  -- Next State Combinational Logic ----------------------------------
+  FSM: process( State, X )
+  begin
+    CASE State IS
+      when state_rx =>
+                 if (X = '1') then
+                   Next_State <= state_md;
+                 else
+                   Next_State <= state_rx;
+                 end if;
+      when state_md =>
+                 if (X = '1') then
+                   Next_State <= state_tx;
+                 else
+                   Next_State <= state_b4;
+                 end if;
+      when state_b4 =>
+                 if (X = '1') then
+                   Next_State <= state_md;
+                 else
+                   Next_State <= state_rx;
+                 end if;
+      when state_tx =>
+                 if (X = '1') then
+                   Next_State <= state_tx;
+                 else
+                   Next_State <= state_b4;
+                 end if;
+    END case;
+  end process;
+
+  -- State Register --------------------------------------------------
+  REG: process( clk, CL )
+  begin
+    if (CL = '0') then
+              State <= state_rx;
+    elsif rising_edge(clk) then
+              State <= Next_State;
+       end if;
+  end process;
+
+  -- Outputs Combinational Logic -----------------------------------
+  OUTPUTS: process( State, X )
+  begin
+    -- Set output defaults:
+    ALU_s <= '0';
+    IOM_s <= '0';
+    COM_s <= '0';
+
+    -- Set output as function of current state and input:
+    CASE State IS
+      when state_rx =>
+                 COM_s <= '1';
+      when state_b4 =>
+                 ALU_s <= '1';
+      when state_tx =>
+                 IOM_s <= '1';
+                 COM_s <= '1';
+      when OTHERS =>
+                 ALU_s <= '0';
+                 IOM_s <= '0';
+                 COM_s <= '0';
+    END case;
+  end process;
+
+END behavioral;
